1a9493488ddd2b03f9c4572f4a9c98c13218297e
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2014-12-29 01:27:35 +0100

    Pointed index.php to the page viewer

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $host = $_SERVER['HTTP_HOST'];
 $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$extra = 'src/html/create-math-problem.html';
+$extra = 'src/html/create-page.html';
 
 header("Location: http://$host$uri/$extra");
 ?>
